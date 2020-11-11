413cec8c8fd164c62469a926869b2ee448cbe565
balbatushka <balbatushka@gmail.com>
2020-11-11 10:02:31 +0300

    feat: added stoires for todo and todolist

--- src/stories/TodoItem.stories.js
+++ src/stories/TodoItem.stories.js
@@ -10,9 +10,15 @@ export default {
 const Template = (args) => <TodoItem {...args} />;
 
 export const Default = Template.bind({});
-
 Default.args = {
   todo: { id: 1, completed: false, title: "buy milk" },
+  index: 1,
+  onChange: false,
+};
+
+export const Done = Template.bind({});
+Done.args = {
+  todo: { id: 1, completed: true, title: "buy milk" },
   index: 1,
   onChange: false,
 };
--- src/stories/TodoList.stories.js
+++ src/stories/TodoList.stories.js
@@ -7,13 +7,25 @@ export default {
 };
 
 const Template = (args) => <TodoList {...args} />;
+const TemplateEmpty = () => <p>No todos</p>;
 
 export const Default = Template.bind({});
-
 Default.args = {
   todos: [
     { id: 1, completed: false, title: "buy milk" },
-    { id: 2, completed: true, title: "buy honey" },
+    { id: 2, completed: false, title: "buy honey" },
     { id: 3, completed: false, title: "buy bun" },
+  ],
+};
+
+export const Empty = TemplateEmpty.bind({});
+
+export const ListWithDoneTodo = Template.bind({});
+
+ListWithDoneTodo.args = {
+  todos: [
+    { id: 1, completed: true, title: "buy milk" },
+    { id: 2, completed: true, title: "buy honey" },
+    { id: 3, completed: true, title: "buy bun" },
   ],
 };
