2b0daf04e3758e10a5dcd0c168b0cfb5e79db39d
balbatushka <balbatushka@gmail.com>
2020-11-17 10:58:54 +0300

    feat: alert for delete todo added

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AlertContext } from "./../context/alert/alertContext";
 
-export const Todos = ({ todos, onRemove }) => (
-  <ul className="list-group">
-    {todos.map((todo) => (
-      <li className="list-group-item todo" key={todo.id}>
-        <div>
-          <strong>{todo.title}</strong>
-          <small>{todo.date}</small>
-        </div>
-        <button
-          type="button"
-          class="btn btn-outline-danger btn-sm"
-          onClick={() => onRemove(todo.id)}
-        >
-          &times;
-        </button>
-      </li>
-    ))}
-  </ul>
-);
+export const Todos = ({ todos, onRemove }) => {
+  const alert = useContext(AlertContext);
+
+  return (
+    <ul className="list-group">
+      {todos.map((todo) => (
+        <li className="list-group-item todo" key={todo.id}>
+          <div>
+            <strong>{todo.title}</strong>
+            <small>{todo.date}</small>
+          </div>
+          <button
+            type="button"
+            className="btn btn-outline-danger btn-sm"
+            onClick={() => {
+              onRemove(todo.id).then(() => {
+                alert.show("Todo was deleted", "warning");
+              });
+            }}
+          >
+            &times;
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
